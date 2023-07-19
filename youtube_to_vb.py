572cf34009a5f941bbe3a99b9c826935bed02312
Nazim-Yoldas Sesen <nazimsesen@gmail.com>
2023-07-18 21:44:23 -0400

    Changed Output Name's logic to Video's Title

--- youtube_to_vb.py
+++ youtube_to_vb.py
@@ -8,12 +8,11 @@ def download_video_as_mp3(url, output_filename):
             'preferredcodec': 'mp3',
             'preferredquality': '0',  # V0 quality
         }],
-        'outtmpl': output_filename
+        'outtmpl': '%(title)s.%(ext)s',
     }
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
 video_url = "YOUR_YOUTUBE_VIDEO_URL_HERE"
-output_filename = "output.mp3"
-download_video_as_mp3(video_url, output_filename)
+download_video_as_mp3(video_url)
